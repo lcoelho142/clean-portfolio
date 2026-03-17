232c23272c8802b13a010406de72a9e323f7b2ea
Leah Coelho <lcoelho142@gmail.com>
2026-03-17 09:23:01 -0400

    layout checkpoint

--- src/components/MidSec.jsx
+++ src/components/MidSec.jsx
@@ -1,11 +1,25 @@
 import './MidSec.css'
 import MyName from './MyName';
+import LeahBloom from '../assets/leah-bloom.png';
+import HLine from './HLine'
 
 export default function MidSec() {
     return (
         <>
             <main className="main-section">
-                <MyName className="svg-name" />
+                <div className="top-group">
+                    <h1><MyName className="svg-name" /></h1>
+                    <p className="main-font">Helping clients build trust with their customers by creating an experience that feels tailored to them.</p>
+                </div>
+
+                <HLine offset="0px" marginY="0px" />
+
+                <div className="top-group">
+                    <h1><MyName className="svg-name" /></h1>
+                    <p className="main-font">Helping clients build trust with their customers by creating an experience that feels tailored to them.</p>
+                </div>
+
+                <img src={LeahBloom} className="leah-bloom" />
             </main>
         </>
     )
