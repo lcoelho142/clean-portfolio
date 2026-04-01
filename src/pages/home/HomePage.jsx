a77ea0659f09bafec7d0d3509bd866c09b390b93
Leah Coelho <lcoelho142@gmail.com>
2026-04-01 15:51:38 -0400

    Adjusted homepage layout

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -23,7 +23,7 @@ export default function HomePage() {
                 <div className="bottom-group">
                     <h2 className="secondary-font text-3xl font-light">A <span className="font-bold-italic" >Front-end</span> Web <span className="font-bold-italic" >Designer</span> & <span className="font-bold-italic" >Developer</span></h2>
                     <p className="main-font text-lg">Providing brand overhauls, website design & development -all packaged into one service. Transforming ideas, observations and understandings into designs that value a company's future vision.</p>
-                    <Link to="/work" className="work-btn animated-underline main-font font-bold accent-color text-lg">
+                    <Link to="/work" className="work-btn animated-underline main-font font-bold accent-color text-xl">
                         see work
                     </Link>
                     
