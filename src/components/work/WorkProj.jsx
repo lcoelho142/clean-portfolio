a851a9bf3176b3c63ecdbd3a03352cbd1ac3885c
Leah Coelho <lcoelho142@gmail.com>
2026-04-09 17:29:00 -0400

    Create Work Page content Layout

---
+++ src/components/work/WorkProj.jsx
@@ -0,0 +1,10 @@
+import './WorkProj.css';
+import HLine from '../../components/common/HLine';
+
+export default function WorkProj() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
--- src/pages/work/WorkPage.jsx
+++ src/pages/work/WorkPage.jsx
@@ -1,11 +1,16 @@
 import Layout from "../../components/layout/Layout";
 import './WorkPage.css';
+import HLine from '../../components/common/HLine';
+import WorkProj from '../work/WorkPage';
 
 export default function WorkPage() {
     return (
         <Layout>
             <main className="main-section">
-                <h1 className="secondary-font">Work Portfolio Coming Soon</h1>
+                <h1 className="secondary-font">Work</h1>  
+                <div>
+                    <WorkProj />
+                </div>
             </main>
         </Layout>
     );
